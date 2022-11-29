10cd7dbf125fec2fbbef7fa9398f27a714864899
Pedro Nunes Machado <100473453+PedroNnsM@users.noreply.github.com>
2022-11-29 13:35:23 -0300

    first commit

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,17 @@
+class UsersController {
+   /**
+    * index - GET para listar varios registros.
+    *  show - GET para exibir um registro especifico.
+    * create - POST para criar um registro.
+    * update - PUT para atualizar um registro.
+    * delete - DELETE para remover um registro. 
+    */
+
+    create (request, response) {
+        const {name, email, password} = request.body
+
+        response.json({name, email, password})
+    }
+}
+
+module.exports = UsersController;
