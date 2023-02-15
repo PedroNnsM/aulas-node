ec604baae17276afa6b5b13b1fcd558485e591ac
PedroNnsM <pedronunes95@outlook.com>
2023-02-14 22:27:26 -0300

    Criando no users no banco de dados

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,22 +1,36 @@
-const AppError = require("../utils/AppError")
+const AppError = require("../utils/AppError");
+
+const sqliteConnection = require("../database/sqlite");
+
 class UsersController {
-   /**
-    * index - GET para listar varios registros.
-    *  show - GET para exibir um registro especifico.
-    * create - POST para criar um registro.
-    * update - PUT para atualizar um registro.
-    * delete - DELETE para remover um registro. 
-    */
+  /**
+   * index - GET para listar varios registros.
+   *  show - GET para exibir um registro especifico.
+   * create - POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
 
-    create (request, response) {
-        const { name, email, password } = request.body
+  async create(request, response) {
+    const { name, email, password } = request.body;
 
-        if(!name){
-            throw new AppError("Nome é obrigatório!")
-        }
+    const database = await sqliteConnection();
 
-        response.status(201).json({name, email, password})
+    const checkUserExists = await database.get(
+      //faz a busca no banco de dados
+      "SELECT * FROM users WHERE email = (?)", [email]
+    );
+
+    if (checkUserExists) {
+      throw new AppError("este email ja esta em uso");
     }
+    // Serve para inserir dados no banco de dados
+    await database.run(
+      "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+      [name, email, password]
+    );
+    return response.status(201).json();
+  }
 }
 
 module.exports = UsersController;
