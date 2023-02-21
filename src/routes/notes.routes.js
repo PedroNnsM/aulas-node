e86b9ad9fee508d42ce80a814ee6a306e3f25b1c
PedroNnsM <pedronunes95@outlook.com>
2023-02-21 20:08:24 -0300

    Rota delete criada

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -15,5 +15,7 @@ const notesController = new NotesController();
 
 notesRoutes.post("/:user_id", notesController.create);
 notesRoutes.get("/:id", notesController.show);
+notesRoutes.delete("/:id", notesController.delete);
+
 
 module.exports = notesRoutes;
