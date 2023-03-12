364de16d34440e6eb1bed9ae3f583bd5224e320e
PedroNnsM <pedronunes95@outlook.com>
2023-03-11 23:18:08 -0300

    middleware ensure criado

---
+++ src/middlewares/ensureAuthenticated.js
@@ -0,0 +1,27 @@
+const { verify } = require("jsonwebtoken");
+const AppError = require("../utils/AppError");
+const authconfig = require("../configs/auth");
+
+function ensureAuthenticated(request, response, next) {
+  const authHeader = request.header.authorization;
+
+  if (!authHeader) {
+    throw new AppError("JWT Token nâo informado", 401);
+  }
+
+  const [, token] = authHeader.split(" ");
+
+  try {
+    const { sub: user_id } = verify(token, authconfig.jwt.secret);
+
+    request.user = {
+      id: Number(user_id),
+    };
+
+    return next();
+  } catch {
+    throw new AppError("JWT Token inválido", 401);
+  }
+}
+
+module.exports = ensureAuthenticated;
