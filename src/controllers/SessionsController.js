e8f3aa14917c1d326881b06990f0de146acca06b
PedroNnsM <pedronunes95@outlook.com>
2023-03-26 22:39:30 -0300

    enviando o token

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -1,6 +1,8 @@
 const knex = require("../database/knex");
 const AppError = require("../utils/AppError");
 const { compare } = require("bcryptjs");
+const authConfig = require("../configs/auth");
+const { sign } = require("jsonwebtoken");
 
 class SessionsController {
   async create(request, response) {
@@ -12,13 +14,19 @@ class SessionsController {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
-    const passwordMatched = await compare(password, user.password)
+    const passwordMatched = await compare(password, user.password);
 
-    if(!passwordMatched){
+    if (!passwordMatched) {
       throw new AppError("E-mail e/ou senha incorreta", 401);
     }
 
-    return response.json(user);
+    const { secret, expiresIn } = authConfig.jwt;
+    const token = sign({}, secret, {
+      subject: String(user.id),
+      expiresIn,
+    });
+
+    return response.json({ user, token });
   }
 }
 
