75a1c9205919e29ba318c3bfd629561d835cb07e
PedroNnsM <pedronunes95@outlook.com>
2023-03-27 23:08:15 -0300

    upload.js

--- src/configs/upload.js
+++ src/configs/upload.js
@@ -1,11 +1,9 @@
 const path = require("path");
-
-const multer = require("multer");
 const crypto = require("crypto");
+const multer = require("multer");
 
 const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp");
-
-const UPLOADS_FOLDER = path.resolve(TMP_FOLDER, "uploads");
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads");
 
 const MULTER = {
   storage: multer.diskStorage({
